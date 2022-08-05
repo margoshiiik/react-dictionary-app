ca979e79bee8e20197b6911b3e329c9a63a02f02
margoshiiik <margo.budenkova@gmail.com>
2022-08-05 14:45:33 +0300

    adding photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, {useState} from "react";
 import "./Dictionary.css"
 import axios from "axios";
 import Definition from "./Definition"
+import Photos from "./Photos"
 
 export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.default); 
     let [result, setResult] = useState(null);
     let [isLoaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function setKeywordOnChange(event){ 
         setKeyword(event.target.value);
@@ -20,11 +22,20 @@ export default function Dictionary(props){
         event.preventDefault();
         search();
       }
+
+      function handleResponsePhotos(response){
+        setPhotos(response.data.photos)
+      }
     
 
     function search(){
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
         axios.get(apiUrl).then(handleResponse)
+
+        let apiKey = '563492ad6f9170000100000199db2bef95a14a59b372485c93690e84'
+        let api = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization : `Bearer ${apiKey}`}; 
+        axios.get(api, {headers: headers }).then(handleResponsePhotos)
     }
 
     function load(){
@@ -46,6 +57,7 @@ export default function Dictionary(props){
                 <span className="hint">Suggested word to search: yoga, dog, hello...</span>
             </section>
              <Definition word={result}/> 
+             <Photos photos={photos}/> 
         </div>
     )}
     else {
