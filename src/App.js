355421cecb614fcc905bc281e0841d5a67ca2737
margoshiiik <margo.budenkova@gmail.com>
2022-08-05 14:49:23 +0300

    changing phonetics

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className='text-center'>Dictionary</h1>
-        <Dictionary default={"sunrise"}/>
+        <Dictionary default={"sunshine"}/>
       </header>
     </div>
   );
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,10 +3,12 @@ import "./Phonetic.css"
 
 export default function Phonetic(props){
     console.log(props.phonetics); 
+    if(props.phonetics.text) {
     return (
         <div className="Phonetic mt-4">
             <a href={props.phonetics.audio} target="_blank">listen</a>
             <b><span>{props.phonetics.text}</span></b>
         </div>
     )
+    }
 }
